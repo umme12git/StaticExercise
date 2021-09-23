e211d7513ca0a994a7a5759915e39250f0174a5b
umme <unnipa01@yahoo.com>
2021-09-23 09:03:52 -0500

    initial copy

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double fahren;
+            double celcius;
+
+            fahren = 85;
+            celcius = TempConverter.FahrenheitToCelcius(fahren);
+            Console.WriteLine($"Temparature in celcius is  {Math.Round(celcius,2)}");
+
+            celcius = 20;
+            fahren = TempConverter.CelciusToFahrenheit(celcius);
+            Console.WriteLine($"Temparature in fahrenheit is {Math.Round(fahren,2)}");
+
+        }
+    }
+}
---
+++ TempConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelcius(double fahrenheit)
+        {
+            double   val = 5 *1f/9*1f;
+            //Console.WriteLine(val);
+            return (fahrenheit -32) * (val);
+
+        }
+
+        public static double CelciusToFahrenheit(double celcius)
+        {
+            double val = 9 * 1f / 5 * 1f;
+            //Console.WriteLine(val);
+            return (celcius * val) + 32;
+        }
+    }
+}
